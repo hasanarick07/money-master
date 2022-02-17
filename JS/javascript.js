2e6c9b958644a35902a1ce334eedd605bf457395
Arick Hasan <hasanarick000@gmail.com>
2022-02-17 14:38:07 +0600

    2nd

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,15 +1,36 @@
-console.log("hello");
+const incomeInput= document.getElementById('incomeInput');
+const foodExpenseInput= document.getElementById('foodExpenseInput');
+const rentExpenseInput= document.getElementById('rentExpenseInput');
+const clothExpenseInput= document.getElementById('clothExpenseInput');
+const totalExpanse = document.getElementById('totalExpanse');
+const balance = document.getElementById('balance');
+const savingAmount = document.getElementById('savingAmount');
+const remainingBalance = document.getElementById('remainingBalance');
 
+
+function addExpense(food, rent, cloth) {
+    return food+rent+cloth;
+}
+function savingAmountCalculation(fullAmount, percentAmount) {
+    const percentAmountValue = parseFloat(percentAmount/100);
+    return fullAmount*percentAmountValue;
+}
 
 document.getElementById('calculateButton').addEventListener('click',function(){
-    const incomeInput= document.getElementById('incomeInput').value;
-    const foodExpenseInput= document.getElementById('foodExpenseInput').value;
-    const rentExpenseInput= document.getElementById('rentExpenseInput').value;
-    const clothExpenseInput= document.getElementById('clothExpenseInput').value;
-    const totalExpanse = document.getElementById('totalExpanse');
-    const balance = document.getElementById('balance');
-    totalExpanse.innerText= Number(foodExpenseInput)+Number(rentExpenseInput)+Number(clothExpenseInput);
-    balance.innerText= Number(incomeInput)-Number(totalExpanse);
+    const incomeInputValue = parseFloat(incomeInput.value);
+    const foodExpenseInputValue = parseFloat(foodExpenseInput.value);
+    const rentExpenseInputValue = parseFloat(rentExpenseInput.value);
+    const clothExpenseInputValue = parseFloat(clothExpenseInput.value);
+    const gotTotalExpanse = addExpense(foodExpenseInputValue, rentExpenseInputValue, clothExpenseInputValue);
+    totalExpanse.innerText=gotTotalExpanse;
+    balance.innerText= incomeInputValue-gotTotalExpanse;
+})
 
-    console.log(totalExpanse);
+document.getElementById('saveButton').addEventListener('click',function(){
+    const savingAmountValue = parseFloat(savingAmount.value);
+    const incomeValueInput = parseFloat(incomeInput.value);
+    const remainingBalanceValue = savingAmountCalculation(incomeValueInput, savingAmountValue);
+    
+    remainingBalance.innerText = remainingBalanceValue;
+
 })
